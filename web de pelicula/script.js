850943b33a982c74b12b96b991a3ea15a679dc4d
David <davidask611@gmail.com>
2024-03-02 21:49:41 -0300

    "catalogo de pelis"

---
+++ web de pelicula/script.js
@@ -0,0 +1,17 @@
+// Definición de variables utilizando camelCase
+let peliculaSeleccionada;
+let listaPeliculas;
+let listaFechas;
+
+// Funciones utilizando camelCase
+function mostrarDetallesPelicula() {
+  // Lógica para mostrar los detalles de la película seleccionada
+}
+
+function filtrarPeliculasPorFecha() {
+  // Lógica para filtrar películas por fecha
+}
+
+function cargarPeliculas() {
+  // Lógica para cargar la lista de películas desde algún origen de datos
+}
